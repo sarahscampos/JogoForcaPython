92f4efd8dd65a1c1e6a86eae169eeb05b17f1195
sarah-campos <sarahstephanycampos@gmail.com>
2023-07-08 16:06:53 -0300

    condicao de vitoria e derrota

--- forca.py
+++ forca.py
@@ -5,24 +5,35 @@ def jogar():
 
 jogar()
 
-palavra_secreta = "python"
-letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+palavra_secreta = "caqui".upper()
+letras_acertadas = ["_" for letra in palavra_secreta]
+
 
 enforcou = False
 acertou = False
+erros = 0
 
 print(letras_acertadas)
 
 while(not enforcou and not acertou):
     chute = input("Qual letra? ")
-    chute = chute.strip() # essas funções de string não alteram a string original/ STRINGS SÃO IMUTÁVEIS
+    chute = chute.strip().upper() # essas funções de string não alteram a string original/ STRINGS SÃO IMUTÁVEIS
     
-    index = 0
-    for letra in palavra_secreta:
-        if(chute.upper() == letra.upper()):
-            letras_acertadas[index] = letra
-        index = index + 1
-       
+    if(chute in palavra_secreta):
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                letras_acertadas[index] = letra
+            index += 1
+    else:
+        erros += 1 
+        print("Poxa, você errou! Faltam {} tentativas.".format(6-erros))  
 
-
+    enforcou = erros == 6
+    acertou = "_" not in letras_acertadas
     print(letras_acertadas)
+
+if(acertou):
+    print("Você ganhou!!!")
+else:
+    print("Você perdeu :( Tente novamente!")
